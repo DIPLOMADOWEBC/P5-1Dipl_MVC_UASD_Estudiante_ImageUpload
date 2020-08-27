51c730bbea7093084f227ce0c0332938028a0fea
Juan Esteban Gonzalez <e-gonzalez@outlook.com>
2020-08-27 08:59:45 -0400

    Agregar archivos de proyecto.

---
+++ P5-1Dipl_MVC_UASD_Estudiante_ImageUpload/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P5_1Dipl_MVC_UASD_Estudiante_ImageUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Estudiantes()
+        {
+            return View();
+        }
+
+        public ActionResult Detalle()
+        {
+            return View();
+        }
+
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                EmpleadosDB1Entities db = new EmpleadosDB1Entities();
+                string ImageName = System.IO.Path.GetFileName(file.FileName);
+                string physicalPath = Server.MapPath("~/Images/" + ImageName);
+                file.SaveAs(physicalPath);
+                tblEstudiante estudiante = new tblEstudiante();
+                estudiante.Nombres = Request.Form["Nombres"];
+                estudiante.Apellidos = Request.Form["Apellidos"];
+                estudiante.Direccion = Request.Form["Direccion"];
+                estudiante.Telefono = Request.Form["Telefono"];
+                estudiante.Cedula = Request.Form["Cedula"];
+                estudiante.ImagenUrl = ImageName;
+                if (TryValidateModel(estudiante))
+                {
+                    db.tblEstudiante.Add(estudiante);
+                    db.SaveChanges();
+                    return RedirectToAction("../home/Detalle/");
+                }
+                else
+                {
+                    return View("../home/Estudiantes", estudiante);
+                }
+            }
+            return RedirectToAction("../home/Detalle/");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ P5-1Dipl_MVC_UASD_Estudiante_ImageUpload/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="P5_1Dipl_MVC_UASD_Estudiante_ImageUpload.MvcApplication" Language="C#" %>
